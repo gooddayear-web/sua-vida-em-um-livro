48ee95f884e5f07bfaf5f3b335b30b94715e22f6
Leonardo Oliveira <gooddayear@gmail.com>
2026-08-23 18:11:11 -0300

    feat: adiciona tag de rastreamento do Google Ads no layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -49,6 +49,20 @@ export default function RootLayout({
       lang="pt-BR"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
+      <head>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-18406777513"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'AW-18406777513');
+            `,
+          }}
+        />
+      </head>
       <body>
         {children}
       </body>
